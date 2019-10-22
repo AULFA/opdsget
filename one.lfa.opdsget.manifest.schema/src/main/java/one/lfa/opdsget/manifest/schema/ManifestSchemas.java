906264a959f884e94634774b8d532597c2b4df8c
Mark Raynsford <code@io7m.com>
2019-10-22 15:36:43 +0100

    Provide an initial manifest schema
    This defines an XSD schema for feed manifest files.
    
    Affects: https://github.com/AULFA/opdsget/issues/14

---
+++ one.lfa.opdsget.manifest.schema/src/main/java/one/lfa/opdsget/manifest/schema/ManifestSchemas.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2019 Mark Raynsford <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+package one.lfa.opdsget.manifest.schema;
+
+import java.io.InputStream;
+
+/**
+ * Functions to retrieve schemas.
+ */
+
+public final class ManifestSchemas
+{
+  private ManifestSchemas()
+  {
+
+  }
+
+  /**
+   * @return The 1.0 schema
+   */
+
+  public static InputStream schema1p0()
+  {
+    return ManifestSchemas.class.getResourceAsStream(
+      "/one/lfa/opdsget/manifest/schema/schema-1.0.xsd");
+  }
+}
